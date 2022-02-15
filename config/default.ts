7a0601f9c25ec8055acd9992de709007eaf8395d
Enoch Klufio <enochmk94@gmail.com>
2022-02-15 21:36:20 +0000

    chore: update config

--- config/default.ts
+++ config/default.ts
@@ -3,10 +3,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-	port: process.env.PORT || 3000,
+	port: process.env.PORT || 5000,
 	env: process.env.NODE_ENV || 'development',
 	jwt: {
-		expiresIn: '3d',
+		expiresIn: '1h',
 		secret: process.env.JWT_SECRET || '12345678',
 	},
 };
